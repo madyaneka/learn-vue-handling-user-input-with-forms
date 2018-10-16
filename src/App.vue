d385fdff290ba91ddd8ff33036ceb26da09c769c
Madyan Eka <madyan.eka95@gmail.com>
2018-10-16 19:38:26 +0700

    creating a custom control (input)

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,8 @@
                                 type="text"
                                 id="email"
                                 class="form-control"
-                                v-model="userData.email">
+                                :value="userData.email"
+                                @input="userData.email = $event.target.value">
                     </div>
                     <div class="form-group">
                         <label for="password">Password</label>
@@ -95,6 +96,11 @@
                     </select>
                 </div>
             </div>
+            <div class="row">
+                 <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                     <app-switch v-model="dataSwitch"></app-switch>
+                 </div>
+            </div>
             <hr>
             <div class="row">
                 <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
@@ -122,7 +128,7 @@
                         </ul>
                         <p>Gender: {{ gender }}</p>
                         <p>Priority: {{ selectedPriority }}</p>
-                        <p>Switched:</p>
+                        <p>Switched: {{ dataSwitch }}</p>
                     </div>
                 </div>
             </div>
@@ -131,7 +137,13 @@
 </template>
 
 <script>
+    import Switch from './Switch.vue'
+
     export default {
+        components: {
+            appSwitch: Switch
+        },
+
         data() {
             return {
                 userData: {
@@ -147,7 +159,8 @@
                     'High',
                     'Medium',
                     'Low'
-                ]
+                ],
+                dataSwitch: true
             }
         }
     }
